e89cb4311d591fda0b4447778ce2886806320ed4
Ahmed Abdullah <akdadoc@gmail.com>
2021-08-31 14:51:42 -0500

    add web interface
    dapp interface for bfd smart contract

---
+++ web/js/mask_module.js
@@ -0,0 +1,126 @@
+/*
+    @Author akdadoc 2020 
+    metamask api
+*/
+
+//  MetaMask Provider
+const provider = await detectEthereumProvider();
+const port = 9500;
+const forwarderOrigin = "http://10.0.0.64:" + port + "/bfd";
+
+if (provider) {
+  console.log("MetaMask is  Installed!");
+} else {
+  console.log("Please install MetaMask!");
+}
+
+const initialize = () => {
+  //  basic button functions
+  const onboardButton = document.getElementById("connectButton");
+  const accountDiv = document.getElementById("account");
+
+  //We create a new MetaMask onboarding object to use in our app
+  const onboarding = new MetaMaskOnboarding({ forwarderOrigin });
+
+  //Created check function to see if the MetaMask extension is installed
+  const isMetaMaskInstalled = () => {
+    //Have to check the ethereum binding on the window object to see if it's installed
+    const { ethereum } = window;
+    return Boolean(ethereum && ethereum.isMetaMask);
+  };
+
+  //This will start the onboarding proccess
+  const onClickInstall = () => {
+    onboardButton.innerText = "Onboarding in progress";
+    onboardButton.disabled = true;
+    //On this object we have startOnboarding which will start the onboarding process for our end user
+    onboarding.startOnboarding();
+  };
+
+  const onClickConnect = async () => {
+    try {
+      // Will open the MetaMask UI
+      // You should disable this button while the request is pending!
+      await ethereum.request({ method: "eth_requestAccounts" });
+    } catch (error) {
+      console.error(error);
+    } finally {
+      // Change button Text!
+      onboardButton.innerText = "Connected";
+      accountDiv.innerText = "Connected Account: " + currentAccount;
+    }
+  };
+
+  /**********************************************************/
+  /* Handle chain (network) and chainChanged (per EIP-1193) */
+  /**********************************************************/
+
+  const chainId = async () => {
+    try {
+      await ethereum.request({ method: "eth_chainId" });
+    } catch (error) {
+      console.error(error);
+    } finally {
+      handleChainChanged(chainId);
+    }
+  };
+
+  ethereum.on("chainChanged", handleChainChanged);
+
+  function handleChainChanged(_chainId) {
+    // We recommend reloading the page, unless you must do otherwise
+    window.location.reload();
+  }
+
+  /***********************************************************/
+  /* Handle user accounts and accountsChanged (per EIP-1193) */
+  /***********************************************************/
+
+  let currentAccount = null;
+  ethereum
+    .request({ method: "eth_accounts" })
+    .then(handleAccountsChanged)
+    .catch((err) => {
+      // Some unexpected error.
+      // For backwards compatibility reasons, if no accounts are available,
+      // eth_accounts will return an empty array.
+      console.error(err);
+    });
+
+  // Note that this event is emitted on page load.
+  // If the array of accounts is non-empty, you're already
+  // connected.
+  ethereum.on("accountsChanged", handleAccountsChanged);
+
+  // For now, 'eth_accounts' will continue to always return an array
+  function handleAccountsChanged(accounts) {
+    if (accounts.length === 0) {
+      // MetaMask is locked or the user has not connected any accounts
+      console.log("Please connect to MetaMask.");
+    } else if (accounts[0] !== currentAccount) {
+      currentAccount = accounts[0];
+    }
+  }
+
+  const MetaMaskClientCheck = () => {
+    //Now we check to see if MetaMask is installed
+    if (!isMetaMaskInstalled()) {
+      //If it isn't installed we ask the user to click to install it
+      onboardButton.innerText = "Click here to install MetaMask!";
+
+      //When the button is clicked we call this function
+      onboardButton.onclick = onClickInstall;
+      //The button is now disabled
+      onboardButton.disabled = false;
+    } else {
+      //If it is installed we change our button text
+      onboardButton.innerText = "Connect Wallet";
+
+      //When the button is clicked we call this function to connect the users MetaMask Wallet
+      onboardButton.onclick = onClickConnect;
+    }
+  };
+  MetaMaskClientCheck();
+};
+
+window.addEventListener("DOMContentLoaded", initialize);
